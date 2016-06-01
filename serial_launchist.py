6a4e07ef15987eac668ed36807335a0ab442b941
James Oliver <joliver@rancher.com>
2016-06-01 14:00:58 -0700

    fixups

--- serial_launchist.py
+++ serial_launchist.py
@@ -18,14 +18,13 @@ import time
 #sudo sysctl -p
 
 # configuration parameters
-start = 171
-stop = 180
-maxprocs = 5
+start = 1
+stop = 1000
 
 # Rancher API credentials
-environ['RANCHER_URL'] = 'http://130.211.199.188:8080/v1/projects/1a5'
-environ['RANCHER_ACCESS_KEY'] = '6C043054EDDEAD42A3C1'
-environ['RANCHER_SECRET_KEY'] = 'ZUAhevS3x9bq4DdXVMB1XVDYnXAYAfVUf8raUxe2'
+environ['RANCHER_URL'] = 'http://104.197.228.222:8080/v1/projects/1a5'
+environ['RANCHER_ACCESS_KEY'] = '3A190E9E34C6B3DBA112'
+environ['RANCHER_SECRET_KEY'] = 'xiBwfMf9QAsdET6ZCftdLJzLABTxbqmo4S89dcZw'
 
 # Template environment
 env = Environment(
@@ -34,13 +33,16 @@ env = Environment(
 
 # scheduling step function
 def delay(id):
-  if   id < 2:   return 60
-  elif id < 50:  return 20
-  elif id < 100: return 25
-  elif id < 150: return 30
-  elif id < 200: return 40
-  elif id < 250: return 50
-  else:          return 60
+  if   id <= 100: return 7
+  elif id <= 200: return 8
+  elif id <= 300: return 9
+  elif id <= 400: return 10
+  elif id <= 500: return 11
+  elif id <= 600: return 12
+  elif id <= 700: return 13
+  elif id <= 800: return 14
+  elif id <= 900: return 15
+  else:           return 16
 
 for id in range(start, stop + 1):
   procs = []
@@ -48,10 +50,9 @@ for id in range(start, stop + 1):
     if filename.endswith('.j2'):
       with open(filename[:-3], 'w') as f:
         template = env.get_template(filename)
-        f.write(template.render(host_port=80+id))
-  procs.append(Popen(['rancher-compose', '--project-name', 'reaction-{0}'.format(id), 'up']))
-  if len(procs) > maxprocs:
-    procs.pop().kill()
+        f.write(template.render(host_port=80+id-1))
+  procs.append(Popen(['rancher-compose', '--project-name', 'reaction-{0}'.format(id), 'up', '-d']))
   time.sleep(delay(id))
   remove('docker-compose.yml')
   remove('rancher-compose.yml')
+
