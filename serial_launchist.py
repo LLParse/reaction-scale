4a20e329fc3d51dce41c5b5230e6fc6af6c2b8ab
James Oliver <joliver@rancher.com>
2016-06-02 11:27:31 -0700

    Add profiling

--- serial_launchist.py
+++ serial_launchist.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from jinja2 import Template, Environment, FileSystemLoader
-from subprocess import Popen
+from subprocess import call
 from os import listdir, remove, environ
-import time
+from time import time, sleep
+import urllib
+import psutil
 
 # Kernel configuration.
 #
@@ -19,40 +21,55 @@ import time
 
 # configuration parameters
 start = 1
-stop = 1000
+stop = 5
 
 # Rancher API credentials
-environ['RANCHER_URL'] = 'http://104.197.228.222:8080/v1/projects/1a5'
-environ['RANCHER_ACCESS_KEY'] = '3A190E9E34C6B3DBA112'
-environ['RANCHER_SECRET_KEY'] = 'xiBwfMf9QAsdET6ZCftdLJzLABTxbqmo4S89dcZw'
+environ['RANCHER_URL'] = 'http://52.53.239.197:8080/v1/projects/1a5'
+environ['RANCHER_ACCESS_KEY'] = '20B8F8646AF81E867021'
+environ['RANCHER_SECRET_KEY'] = 'oroS2frKbbomFoK3WCg9XMfUcPwhHrvjB9mBDhyA'
+
+app_host = '127.0.0.1'
+result_file = 'result.csv'
 
 # Template environment
 env = Environment(
   loader=FileSystemLoader('templates'),
   trim_blocks=True)
 
-# scheduling step function
-def delay(id):
-  if   id <= 100: return 7
-  elif id <= 200: return 8
-  elif id <= 300: return 9
-  elif id <= 400: return 10
-  elif id <= 500: return 11
-  elif id <= 600: return 12
-  elif id <= 700: return 13
-  elif id <= 800: return 14
-  elif id <= 900: return 15
-  else:           return 16
-
+samples=[]
 for id in range(start, stop + 1):
-  procs = []
+  app_port = 80 + id - 1
+
   for filename in listdir('templates'):
     if filename.endswith('.j2'):
       with open(filename[:-3], 'w') as f:
         template = env.get_template(filename)
-        f.write(template.render(host_port=80+id-1))
-  procs.append(Popen(['rancher-compose', '--project-name', 'reaction-{0}'.format(id), 'up', '-d']))
-  time.sleep(delay(id))
-  remove('docker-compose.yml')
-  remove('rancher-compose.yml')
+        f.write(template.render(host_port=app_port))
 
+  # sample start time
+  start=time()
+  
+  # Wait for containers to become active
+  call(['rancher-compose', '--project-name', 'reaction-{0}'.format(id), 'up', '-d'])
+  active_time = time() - start
+
+  print 'Waiting for app to accept HTTP requests'
+  while True:
+    try:
+      urllib.urlopen('http://{0}:{1}'.format(app_host, app_port))
+      break
+    except:
+      sleep(.5)
+  up_time = time() - start
+
+  print 'Sampling host cpu/memory'
+  cpu_percent = psutil.cpu_percent(interval=1)
+  vmem_percent = psutil.virtual_memory().percent
+  smem_percent = psutil.swap_memory().percent
+
+  with open(result_file, 'a') as f:
+    samples.append((id, active_time, up_time, cpu_percent, vmem_percent, smem_percent))
+    f.write('{0},{1},{2},{3},{4},{5}'.format(id, active_time, up_time, cpu_percent, vmem_percent, smem_percent))
+
+for sample in samples:
+  print sample
